0e0f7b551984748afbe2b48ea45c45fc62b6a4a4
hoyangtsai <hoyangtsai@users.noreply.github.com>
2023-04-08 07:17:19 +0000

    deploy: f0d808f451409a418223e911e9135e7c294d6c2a

--- index.2736f766.js
+++
@@ -1,2 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(()=>{navigator.serviceWorker.register("/sw.js").then((e=>{console.log("Service worker registered successfully:",e)})).catch((e=>{console.error("Service worker registration failed:",e)}))}));
-//# sourceMappingURL=index.2736f766.js.map
